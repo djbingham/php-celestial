71dd6189dc62db8cc27028a4ce8aca176e4d7488
David Bingham <david.bingham@flashtalking.com>
2015-09-26 18:38:56 +0100

    Converted indentation to tabs in SubJoinList definition in Graph module.

--- Module/Graph/Definition/Table/Join/SubJoinList.php
+++ Module/Graph/Definition/Table/Join/SubJoinList.php
@@ -5,39 +5,40 @@ use Sloth\Helper\ObjectList;
 
 class SubJoinList extends ObjectList
 {
-    public function push(SubJoin $view)
-    {
-        $this->items[] = $view;
-        return $this;
-    }
+	public function push(SubJoin $view)
+	{
+		$this->items[] = $view;
+		return $this;
+	}
 
-    /**
-     * @return SubJoin
-     */
-    public function shift()
-    {
-        $item = array_shift($this->items);
-        return $item;
-    }
+	/**
+	 * @return SubJoin
+	 */
+	public function shift()
+	{
+		$item = array_shift($this->items);
+		return $item;
+	}
 
-    /**
-     * @param string $index
-     * @return SubJoin
-     */
-    public function getByIndex($index)
-    {
-        return parent::getByIndex($index);
-    }
+	/**
+	 * @param string $index
+	 * @return SubJoin
+	 */
+	public function getByIndex($index)
+	{
+		return parent::getByIndex($index);
+	}
 
-    public function getByParentTableAlias($parentAlias)
-    {
-        $foundJoin = null;
-        foreach ($this as $join) {
-            /** @var SubJoin $join */
-            if ($join->parentTable->getAlias() === $parentAlias) {
-                $foundJoin = $join;
-            }
-        }
-        return $foundJoin;
-    }
+	public function getByParentTableAlias($parentAlias)
+	{
+		$foundJoin = null;
+		foreach ($this as $join) {
+			/** @var SubJoin $join */
+			if ($join->parentTable->getAlias() === $parentAlias) {
+				$foundJoin = $join;
+				break;
+			}
+		}
+		return $foundJoin;
+	}
 }
