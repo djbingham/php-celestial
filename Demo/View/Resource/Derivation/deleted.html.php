661e306d67fcd309183d32441eed07246614b530
David Bingham <david.bingham@flashtalking.com>
2015-09-29 13:36:05 +0100

    Removed Resource module. It no longer works and the Graph module now implements all of the functionality that ever worked in Resource module, apart from delete, so there is no point keeping this here.

--- Demo/View/Resource/Derivation/deleted.html.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * @var Sloth\Module\Resource\Base\Resource $resource
- */
-?>
-<h2>Derivation <?= $resource->getAttribute('id') ?> Deleted</h2>
-<h3><?= $resource->getAttribute('name') ?></h3>
-<p><?= $resource->getAttribute('description') ?></p>
--- Module/Resource/Base/ParsedRequest.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Sloth\Module\Resource\Base;
-
-interface ParsedRequest
-{
-	public function __construct(array $properties);
-}
