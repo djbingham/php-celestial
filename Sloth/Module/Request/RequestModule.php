56d85584bdb27b96f5d4034716745f274146900f
David Bingham <david.bingham@flashtalking.com>
2015-10-27 14:01:45 +0000

    Added caching of request from server vars to Request module.

--- Sloth/Module/Request/RequestModule.php
+++ Sloth/Module/Request/RequestModule.php
@@ -1,32 +1,40 @@
 <?php
 namespace Sloth\Module\Request;
 
+use Helper\InternalCacheTrait;
+
 class RequestModule
 {
+	use InternalCacheTrait;
+
 	public function fromServerVars()
 	{
-		$requestUri = urldecode($_SERVER['REQUEST_URI']);
-		$urlParts = parse_url(urldecode($requestUri));
-		if (!array_key_exists('query', $urlParts)) {
-			$urlParts['query'] = '';
+		if (!$this->isCached('fromServerVars')) {
+			$requestUri = urldecode($_SERVER['REQUEST_URI']);
+			$urlParts = parse_url(urldecode($requestUri));
+			if (!array_key_exists('query', $urlParts)) {
+				$urlParts['query'] = '';
+			}
+			if (!array_key_exists('fragment', $urlParts)) {
+				$urlParts['fragment'] = '';
+			}
+			$properties = array(
+				'method' => strtolower($_SERVER['REQUEST_METHOD']),
+				'uri' => urldecode($requestUri),
+				'path' => $urlParts['path'],
+				'queryString' => $urlParts['query'],
+				'fragment' => $urlParts['fragment'],
+				'params' => array(
+					'get' => $_GET,
+					'post' => $_POST,
+					'cookie' => $_COOKIE,
+					'server' => $_SERVER
+				)
+			);
+			$request = $this->fromArray($properties);
+			$this->setCached('fromServerVars', $request);
 		}
-		if (!array_key_exists('fragment', $urlParts)) {
-			$urlParts['fragment'] = '';
-		}
-		$properties = array(
-			'method' => strtolower($_SERVER['REQUEST_METHOD']),
-			'uri' => urldecode($requestUri),
-			'path' => $urlParts['path'],
-			'queryString' => $urlParts['query'],
-			'fragment' => $urlParts['fragment'],
-			'params' => array(
-				'get' => $_GET,
-				'post' => $_POST,
-				'cookie' => $_COOKIE,
-				'server' => $_SERVER
-			)
-		);
-		return $this->fromArray($properties);
+		return $this->getCached('fromServerVars');
 	}
 
 	public function fromArray(array $properties)
