a377c1c1a0f2d09e765c2ffcaeba4bbe9067a2f0
David Bingham <opensource@davidjbingham.co.uk>
2016-04-25 15:50:27 +0100

    Refactored Mustache rendering engine to cache the Mustache_Engine instance. Also moved loading the template content into a protected method, for ease of overwriting if the rendering engine class is extended.

--- Sloth/Module/Render/Engine/Mustache.php
+++ Sloth/Module/Render/Engine/Mustache.php
@@ -5,15 +5,29 @@ use Sloth\Module\Render\Face\RenderEngineInterface;
 
 class Mustache implements RenderEngineInterface
 {
+	/**
+	 * @var \Mustache_Engine
+	 */
+	private $mustache;
+
 	public function render($viewPath, array $parameters = array())
 	{
+		if (!isset($this->mustache)) {
+			$this->mustache = new \Mustache_Engine();
+		}
+
+		$template = $this->getTemplate($viewPath);
+
+		return $this->mustache->render($template, $parameters);
+	}
+
+	protected function getTemplate($viewPath)
+	{
 		ob_start();
-		extract($parameters);
 		require $viewPath;
-		$output = ob_get_contents();
+		$template = ob_get_contents();
 		ob_clean();
-		$mustache = new \Mustache_Engine();
-		$output = $mustache->render($output, $parameters);
-		return $output;
+
+		return $template;
 	}
 }
